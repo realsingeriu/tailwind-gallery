6fd1cdaf11499e3dc8d76134dfc1b3863f690012
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-20 12:30:35 +0900

    이미지 검색 설정 및 이미지 없을 경우 이미지 없음 문구 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState("flowers");
 
   useEffect(() => {
@@ -13,11 +13,15 @@ function App() {
       .then((res) => res.json())
       .then((data) => setImages(data.hits))
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   // tailwindcss(최신) 프로그램으로 app.css는 불필요 삭제
   return (
     <div className="container mx-auto my-7">
+      {images && images.length === 0 && (
+        <h1 className="text-5xl text-center mt-32">이미지가 없습니다.</h1>
+      )}
+      <ImageSearch setTerm={setTerm} />
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-7">
         {images.map((image) => (
           <ImageCard key={image.id} image={image} />
