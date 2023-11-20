7e01e3fb7380f3e285ea0ab95f5c3cd22bee4e02
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-20 09:38:12 +0900

    테일윈드 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">앱 시작</div>;
+  // tailwindcss(최신) 프로그램으로 app.css는 불필요 삭제
+  return <h1 className="text-3xl font-semibold underline">Hello world!</h1>;
 }
 
 export default App;
