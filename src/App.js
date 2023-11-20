0b5b8e60608ae0efdc60a214c73411620cbf84b1
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-20 10:15:53 +0900

    카드만들기 & 픽사베이키 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import "./App.css";
-
 function App() {
   // tailwindcss(최신) 프로그램으로 app.css는 불필요 삭제
-  return <h1 className="text-3xl font-semibold underline">Hello world!</h1>;
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg ">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by 홍길동
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong> 4000
+          </li>
+          <li>
+            <strong>Downloads: </strong> 300
+          </li>
+          <li>
+            <strong>Likes: </strong> 400
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag2
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #tag3
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
